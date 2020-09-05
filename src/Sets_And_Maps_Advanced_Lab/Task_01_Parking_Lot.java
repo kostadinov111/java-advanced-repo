711071b31d6803bb0143df532c1b34305a2712c5
Georgi Kostadinov <kostadinov7@gmail.com>
2020-09-05 23:35:40 +0300

    Add Task 01

--- src/Sets_And_Maps_Advanced_Lab/Task_01_Parking_Lot.java
+++ src/Sets_And_Maps_Advanced_Lab/Task_01_Parking_Lot.java
@@ -1,13 +1,37 @@
 package Sets_And_Maps_Advanced_Lab;
 
+import java.util.LinkedHashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Task_01_Parking_Lot {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
-        
+        Set<String> parkingLot = new LinkedHashSet<>();
+
+        String input;
+
+        while (!"END".equals(input = sc.nextLine())) {
+            String[] tokens = input.split(",\\s+");
+            String token = tokens[0];
+            String carNumber = tokens[1];
+
+            if ("IN".equals(token)) {
+                parkingLot.add(carNumber);
+            } else if ("OUT".equals(token)) {
+                parkingLot.remove(carNumber);
+            }
+        }
+
+        if (parkingLot.isEmpty()) {
+            System.out.println("Parking Lot is Empty");
+        } else {
+            for (String carNumber : parkingLot) {
+                System.out.println(carNumber);
+            }
+        }
 
     }
 }
