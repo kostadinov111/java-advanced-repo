dd168fdfb8ee6a0577ace7767fb7e3034356b0b5
Georgi Kostadinov <kostadinov7@gmail.com>
2020-09-05 23:11:20 +0300

    Add Java files

---
+++ src/Sets_And_Maps_Advanced_Lab/Task_01_Parking_Lot.java
@@ -0,0 +1,13 @@
+package Sets_And_Maps_Advanced_Lab;
+
+import java.util.Scanner;
+
+public class Task_01_Parking_Lot {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        
+
+    }
+}
