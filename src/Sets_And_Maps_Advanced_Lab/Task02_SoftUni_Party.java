c9182b6d6b52fb07f9b7f0b07275abb8f7480283
Georgi Kostadinov <kostadinov7@gmail.com>
2020-09-06 23:01:32 +0300

    Add Task 02

---
+++ src/Sets_And_Maps_Advanced_Lab/Task02_SoftUni_Party.java
@@ -0,0 +1,49 @@
+package Sets_And_Maps_Advanced_Lab;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Task02_SoftUni_Party {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        Set<String> reservationsVIP =  new HashSet<>();
+        Set<String> reservationsRegular =  new HashSet<>();
+
+        int reservationNumbersLen = 8;
+
+        String input;
+
+        while (!"PARTY".equals(input = sc.nextLine())) {
+            char firstSymbol = input.charAt(0);
+            int reservationLen = input.length();
+
+            if (Character.isDigit(firstSymbol) && reservationLen == reservationNumbersLen) {
+                reservationsVIP.add(input);
+            } else if (reservationLen == reservationNumbersLen) {
+                reservationsRegular.add(input);
+            }
+
+        }
+
+        while (!"END".equals(input = sc.nextLine())) {
+            reservationsVIP.remove(input);
+            reservationsRegular.remove(input);
+        }
+
+        System.out.println(reservationsVIP.size() + reservationsRegular.size());
+
+        reservationsVIP
+                .stream()
+                .sorted(String::compareTo)
+                .forEach(System.out::println);
+
+        reservationsRegular
+                .stream()
+                .sorted(String::compareTo)
+                .forEach(System.out::println);
+
+    }
+}
