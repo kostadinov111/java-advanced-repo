0f4f2b97928be8106dbda76dc79f338c49313589
Georgi Kostadinov <kostadinov7@gmail.com>
2020-09-07 10:32:44 +0300

    Update Task_04_Count_Real_Numbers.java

--- src/Sets_And_Maps_Advanced_Lab/Task_04_Count_Real_Numbers.java
+++ src/Sets_And_Maps_Advanced_Lab/Task_04_Count_Real_Numbers.java
@@ -1,5 +1,8 @@
 package Sets_And_Maps_Advanced_Lab;
 
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Task_04_Count_Real_Numbers {
@@ -7,7 +10,23 @@ public class Task_04_Count_Real_Numbers {
 
         Scanner sc = new Scanner(System.in);
 
-        
+        Map<Double, Integer> mapCountRealNumbers = new LinkedHashMap<>();
 
+        readRealNumbers(sc, mapCountRealNumbers);
+        printRealNumbers(mapCountRealNumbers);
+
+    }
+
+    private static void printRealNumbers(Map<Double, Integer> mapCountRealNumbers) {
+        mapCountRealNumbers
+                .forEach((key, value) -> System.out.println(String.format("%.1f -> %d", key, value)));
+    }
+
+    private static void readRealNumbers(Scanner sc, Map<Double, Integer> mapCountRealNumbers) {
+        double[] realNumbers = Arrays.stream(sc.nextLine().split("\\s+")).mapToDouble(Double::parseDouble).toArray();
+        for (double realNumber : realNumbers) {
+            mapCountRealNumbers.putIfAbsent(realNumber, 0);
+            mapCountRealNumbers.put(realNumber, mapCountRealNumbers.get(realNumber) + 1);
+        }
     }
 }
