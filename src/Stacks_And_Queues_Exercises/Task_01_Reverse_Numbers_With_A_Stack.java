a1111c114b9882102d6ea16aa4f4b2f375ecdea7
Georgi Kostadinov <kostadinov7@gmail.com>
2020-09-15 21:01:50 +0300

    Add Task 01

---
+++ src/Stacks_And_Queues_Exercises/Task_01_Reverse_Numbers_With_A_Stack.java
@@ -0,0 +1,13 @@
+package Stacks_And_Queues_Exercises;
+
+import java.util.Scanner;
+
+public class Task_01_Reverse_Numbers_With_A_Stack {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        
+
+    }
+}
