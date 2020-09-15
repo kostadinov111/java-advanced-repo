57685619f0fd38376ce5501f025ed490ed850384
Georgi Kostadinov <kostadinov7@gmail.com>
2020-09-15 21:13:42 +0300

    Update Task 01

--- src/Stacks_And_Queues_Exercises/Task_01_Reverse_Numbers_With_A_Stack.java
+++ src/Stacks_And_Queues_Exercises/Task_01_Reverse_Numbers_With_A_Stack.java
@@ -1,5 +1,8 @@
 package Stacks_And_Queues_Exercises;
 
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
 import java.util.Scanner;
 
 public class Task_01_Reverse_Numbers_With_A_Stack {
@@ -7,7 +10,27 @@ public class Task_01_Reverse_Numbers_With_A_Stack {
 
         Scanner sc = new Scanner(System.in);
 
-        
+        Deque<Integer> stack = new ArrayDeque<>();
 
+        readNumbers(sc, stack);
+        printNumbers(stack);
+
+    }
+
+    private static void printNumbers(Deque<Integer> stack) {
+        StringBuilder output = new StringBuilder();
+
+        stack
+                .forEach(number -> output.append(String.format("%d ", stack.pop())));
+
+        System.out.println(output.toString().trim());
+    }
+
+    private static void readNumbers(Scanner sc, Deque<Integer> stack) {
+        int[] numbers = Arrays.stream(sc.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
+
+        for (int number : numbers) {
+            stack.push(number);
+        }
     }
 }
