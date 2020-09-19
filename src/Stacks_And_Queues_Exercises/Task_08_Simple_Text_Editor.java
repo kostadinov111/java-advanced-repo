a8757b1b80b873f3cdba21291b3c39455b9d02b6
Georgi Kostadinov <kostadinov7@gmail.com>
2020-09-20 00:08:18 +0300

    Add Task 08

---
+++ src/Stacks_And_Queues_Exercises/Task_08_Simple_Text_Editor.java
@@ -0,0 +1,56 @@
+package Stacks_And_Queues_Exercises;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
+
+public class Task_08_Simple_Text_Editor {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        Deque<String> commandsLog = new ArrayDeque<>();
+
+        readInputAndProcessCommands(sc, commandsLog);
+
+    }
+
+    private static void readInputAndProcessCommands(Scanner sc, Deque<String> commandsLog) {
+        int n = Integer.parseInt(sc.nextLine());
+        String text = "";
+        commandsLog.push(text);
+
+        while (n-- > 0) {
+            String[] tokens = sc.nextLine().split("\\s+");
+            String command = tokens[0];
+
+            switch (command) {
+                case "1": {
+                    String addedText = tokens[1];
+                    text += addedText;
+                    commandsLog.push(text);
+                }
+                break;
+                case "2": {
+                    int count = Integer.parseInt(tokens[1]);
+                    text = text.substring(0, text.toString().length() - count);
+                    commandsLog.push(text);
+                }
+                break;
+                case "3": {
+                    int index = Integer.parseInt(tokens[1]);
+                    System.out.println(String.format("%c", text.charAt(index - 1)));
+                }
+                break;
+                case "4": {
+                    commandsLog.pop();
+                    text = commandsLog.peek();
+                }
+                break;
+                default:
+                    throw new IllegalStateException("Unknown value of tokens[0] -> " + tokens[0]);
+            }
+        }
+
+    }
+}
