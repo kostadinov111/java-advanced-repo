8aa8408d547f0a9845dac98f1aeb6b43054fbc89
Georgi Kostadinov <kostadinov7@gmail.com>
2020-09-24 20:49:18 +0300

    Add Task 05

---
+++ src/Streams_Files_And_Directories_Exercises/Tasks/Task_05_Line_Numbers.java
@@ -0,0 +1,27 @@
+package Streams_Files_And_Directories_Exercises.Tasks;
+
+import java.io.*;
+
+public class Task_05_Line_Numbers {
+    public static void main(String[] args) {
+
+        String inputPath = "src/Streams_Files_And_Directories_Exercises/Resources/inputLineNumbers.txt";
+        String outputPath = "src/Streams_Files_And_Directories_Exercises/output.txt";
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(inputPath));
+             PrintWriter printWriter = new PrintWriter(new FileOutputStream(outputPath))) {
+            int index = 0;
+
+            String line = bufferedReader.readLine();
+
+            while (null != line) {
+                index++;
+                printWriter.println(String.format("%d. %s", index, line));
+                line = bufferedReader.readLine();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
