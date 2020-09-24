763664317cbd661a588b26098054022c024ecc58
Georgi Kostadinov <kostadinov7@gmail.com>
2020-09-24 15:13:56 +0300

    Update Task 01

--- src/Streams_Files_And_Directories_Exercises/Tasks/Task_01_Sum_Lines.java
+++ src/Streams_Files_And_Directories_Exercises/Tasks/Task_01_Sum_Lines.java
@@ -1,9 +1,34 @@
 package Streams_Files_And_Directories_Exercises.Tasks;
 
+import java.io.*;
+
 public class Task_01_Sum_Lines {
     public static void main(String[] args) {
 
-        //TODO
+        String inputPath = "src/Streams_Files_And_Directories_Exercises/Resources/input.txt";
+        String outputPath = "src/Streams_Files_And_Directories_Exercises/output.txt";
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(inputPath));
+             PrintWriter printWriter = new PrintWriter(new FileOutputStream(outputPath))) {
+
+            String line = bufferedReader.readLine();
+
+            while (null != line) {
+                int sum = 0;
+
+                for (char symbol : line.toCharArray()) {
+                    sum += symbol;
+                }
+
+                printWriter.println(sum);
+
+                line = bufferedReader.readLine();
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
