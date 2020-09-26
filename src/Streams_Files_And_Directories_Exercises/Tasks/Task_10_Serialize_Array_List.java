332a057ea11245d0b22f4c2b1f6d5eb505b5cd98
Georgi Kostadinov <kostadinov7@gmail.com>
2020-09-26 16:49:57 +0300

    Update Task 10

--- src/Streams_Files_And_Directories_Exercises/Tasks/Task_10_Serialize_Array_List.java
+++ src/Streams_Files_And_Directories_Exercises/Tasks/Task_10_Serialize_Array_List.java
@@ -10,7 +10,7 @@ public class Task_10_Serialize_Array_List {
         String outputPath = "src/Streams_Files_And_Directories_Exercises/list.ser";
 
         List<Double> numbers = new ArrayList<>();
-        //adding few double values
+        // adding few double values
         numbers.add(5.0d);
         numbers.add(7.2d);
 
