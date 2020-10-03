d68e12e7d2befb26b1a8bf9a2fd6dff53af935de
Georgi Kostadinov <kostadinov7@gmail.com>
2020-10-03 22:01:18 +0300

    Update Task 03

--- src/Defining_Classes_Exercises/Task_03_Speed_Racing/Main.java
+++ src/Defining_Classes_Exercises/Task_03_Speed_Racing/Main.java
@@ -8,7 +8,6 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-
         Map<String, Car> carsInfo = new LinkedHashMap<>();
 
         readCarInput(sc, carsInfo);
