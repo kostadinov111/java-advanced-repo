7144e6fb17adcdf2dc80ed4745add3d7a68f6691
Georgi Kostadinov <kostadinov7@gmail.com>
2020-10-07 13:09:28 +0300

    Add Task 06

---
+++ src/Defining_Classes_Exercises/Task_06_Pokemon_Trainer/Main.java
@@ -0,0 +1,7 @@
+package Defining_Classes_Exercises.Task_06_Pokemon_Trainer;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/Defining_Classes_Exercises/Task_06_Pokemon_Trainer/Pokemon.java
@@ -0,0 +1,5 @@
+package Defining_Classes_Exercises.Task_06_Pokemon_Trainer;
+
+public class Pokemon {
+
+}
